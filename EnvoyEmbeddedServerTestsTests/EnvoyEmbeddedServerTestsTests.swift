65365b11531fb2fdcbc21625412a64b6c4b942fe
Emma Walker <walkere@tcd.ie>
2020-09-16 11:26:27 +0100

    get up to date

--- EnvoyEmbeddedServerTestsTests/EnvoyEmbeddedServerTestsTests.swift
+++ EnvoyEmbeddedServerTestsTests/EnvoyEmbeddedServerTestsTests.swift
@@ -8,27 +8,35 @@
 
 import XCTest
 @testable import EnvoyEmbeddedServerTests
+import Embassy
+import EnvoyAmbassador
 
 class EnvoyEmbeddedServerTestsTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testDownloadFruitData() {
+        
+        // Create an expectation for a background download task.
+        let expectation = XCTestExpectation(description: "Download fruit Data")
+        
+        // Create a URL for a web page to be downloaded.
+        let url = URL(string: "http://[::1]:8000/fruitData")!
+        
+        // Create a background task to download the web page.
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            // Make sure we downloaded some data.
+            XCTAssertNotNil(data, "No data was downloaded.")
+            
+            // Fulfill the expectation to indicate that the background task has finished successfully.
+            expectation.fulfill()
+            
         }
+        
+        // Start the download task.
+        dataTask.resume()
+        
+        // Wait until the expectation is fulfilled, with a timeout of 10 seconds.
+        wait(for: [expectation], timeout: 10.0)
     }
-
+    
 }
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'EnvoyEmbeddedServerTests' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for EnvoyEmbeddedServerTests
+pod 'EnvoyAmbassador', '~> 4.0'
+pod 'Embassy', '~> 4.1'
+
+  target 'EnvoyEmbeddedServerTestsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'EnvoyEmbeddedServerTestsUITests' do
+    # Pods for testing
+  end
+
+end
